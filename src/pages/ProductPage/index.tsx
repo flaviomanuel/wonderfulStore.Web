9bc8933e2c478c7c0e3c13ef5f94c1ec6713aabc
Flávio Manuel <flaviomanuel258@gmail.com>
2023-07-04 22:37:40 -0300

    feat: create ProductPage

---
+++ src/pages/ProductPage/index.tsx
@@ -0,0 +1,15 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Link, Typography } from "@mui/material";
+import Header from "../../Components/Header";
+import OneProduct from "../../Components/OneProduct";
+
+const ProductPage = () => {
+
+    return (
+        <>
+            <Header/>
+            <OneProduct/>
+        </>
+    )
+}
+
+export default ProductPage;
