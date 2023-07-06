359d998f290b693334b6e79ad25b2c10f3ff872e
Flávio Manuel <flaviomanuel258@gmail.com>
2023-07-05 21:04:30 -0300

    remove commented code

--- src/Components/CardProduct/index.tsx
+++ src/Components/CardProduct/index.tsx
@@ -43,25 +43,3 @@ function CardProduct({id, name, price, description, promotionType} : IProduct) {
 }
 
 export default CardProduct;
-// return (
-//     <Card sx={{  width:300,height: '100%', display: 'flex', flexDirection: 'column', margin: 3 }}>
-//         <CardMedia
-//             component="img"
-//             alt="green iguana"
-//             height="140"
-//             image="https://images.unsplash.com/photo-1585583323619-327e6177d23b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
-//         />
-//         <CardContent sx={{ flexGrow: 1 }}>
-//             <Typography gutterBottom variant="h5" component="div">
-//             Caixa de papelação com gato
-//             </Typography>
-//             <Typography variant="body2" color="text.secondary">
-//             Lizards are a widespread group of squamate reptiles, with over 6,000
-//             species, ranging across all continents except Antarctica
-//             </Typography>
-//         </CardContent>
-//         <CardActions>
-//             <Button size="small">Comprar</Button>
-//         </CardActions>
-//     </Card>
-//   );
