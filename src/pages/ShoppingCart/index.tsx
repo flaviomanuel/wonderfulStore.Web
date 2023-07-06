c6ee19872296261a03fbb8fe2f2cb5e3703e4493
Flávio Manuel <flaviomanuel258@gmail.com>
2023-07-05 22:22:21 -0300

    feat: getting AllProductsFromShoppingCartById

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,21 +1,72 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import Header from "../../Components/Header";
-import CardProduct from "../../Components/CardProduct";
 import ShoppingCartCardProduct from "../../Components/ShoppingCartCardProduct";
+import { PromotionTypes } from "../../shared/types";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { IProduct } from "../../shared/interfaces";
+import { useNavigate } from "react-router-dom";
 
+
+const initialValuesProduct ={
+  product : { 
+    id: '',
+    name: '',
+    price: 0,
+    description: '',
+    promotionType: 0 as PromotionTypes
+},
+quantity: 0,
+totalPrice: 0
+}
+interface IShoppingCart  {
+    product:  IProduct,
+    quantity: number,
+    totalPrice: number
+}
+
+
+
+
+
+const  existShoppingCartCreated = localStorage.getItem("idShoppingCart");
 
 const ShoppingCart = () => {
 
-  var products = [1, 2, 3]
+    const navigate = useNavigate();
+
+    const [productsCart, setProductsCart] = useState<IShoppingCart[]>([initialValuesProduct])
+
+    const GetAllProductsFromShoppingCart = async () => {
+        try {
+
+            const response = await api.get(`/ShoppingCartProduct/GetAllProductsFromShoppingCartById?IdShoppingCart=${existShoppingCartCreated}`);
+            setProductsCart(response.data);
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    useEffect(() => {
+        if(existShoppingCartCreated){
+            GetAllProductsFromShoppingCart();
+        }else {
+            alert("Crie se carrinho de compras")
+            navigate('/')
+        }
+    }, [])
 
     return (
         <>
-
         <Header/>
-     
             <Container  sx={{ py: 8 }} maxWidth="sm">
-                {products.map(product => (
-                    <ShoppingCartCardProduct key={product}/>
+                {productsCart.map(productCart => (
+                    <ShoppingCartCardProduct 
+                    key={productCart.product.id} 
+                    quantity={productCart.quantity}
+                    totalPrice={productCart.totalPrice}
+                    product={productCart.product}
+                    />
                 ))}
                 
             </Container>
